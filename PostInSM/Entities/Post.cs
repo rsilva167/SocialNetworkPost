f9e069e5757931af508eb8b1661cb59a836ef0b4
Renan Santos <dosantosrenansilva@gmail.com>
2022-09-07 20:46:06 -0300

    This program simulates a post on a social network, with the post date, title and post content. There is also a space for comments and likes.

---
+++ PostInSM/Entities/Post.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostInSM.Entities
+{
+    internal class Post
+    {
+        public DateTime Time { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int Likes { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime time, string title, string content, int likes)
+        {
+            Time = time;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComments (Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComments (Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Time.ToString("dd/mm/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Coments: ");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ PostInSM/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostInSM.Entities;
+
+namespace PostInSM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip.");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(
+            DateTime.Parse("07/09/2022 20:22:33"),
+            "Traveling to Brazil!",
+            "I'm going to visit this wonderfull country!",
+            12);
+
+            p1.AddComments(c1);
+            p1.AddComments(c2);
+
+            Comment c3 = new Comment("Good night.");
+            Comment c4 = new Comment("May the force be with you!");
+            Post p2 = new Post(
+            DateTime.Parse("06/09/2022 18:30:17"),
+            "Good night guys",
+            "See you tomorrow.",
+            5);
+
+            p2.AddComments(c3);
+            p2.AddComments(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+        }
+    }
+}
